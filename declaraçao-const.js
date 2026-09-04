51d49d37f009a28095d272e4c36128c93b52c02f
Rafapacifico-OSS <rafaelpacificodossantos7@gmail.com>
2026-09-04 16:56:58 -0400

    add-declaraçao-const.js

---
+++ declaraçao-const.js
@@ -0,0 +1,11 @@
+function exemploConst() {
+    const x = 10;
+    console.log(x) 
+    // x = 20; // isso causara um erro porque x foi declarado como const
+    if (true) {
+        const x = 30
+        console.log(y) // 30
+    }
+    // console.log(y); // isso causara um erro por que o Y nao existe fora do bloco
+}
+exemploVar();
