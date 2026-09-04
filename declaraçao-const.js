0bc7f992300aabe3b3e64dea4c22e31ff4be535e
Rafapacifico-OSS <rafaelpacificodossantos7@gmail.com>
2026-09-04 16:48:30 -0400

    add-declaraçao-const.js

--- declaraçao-const.js
+++ declaraçao-const.js
@@ -1,7 +1,9 @@
 function exemploVar() {
+function exemploConst() {
     const x = 10;
     console.log(x) 
     //isso causara um erro porque x foi declarado como const
+    // x = 20; // isso causara um erro porque x foi declarado como const
     if (true) {
         const x = 30
         console.log(y) // 30
