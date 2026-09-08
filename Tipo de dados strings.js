d0966a38dc30026e4b8496fe1982ed03bd86bf44
Rafapacifico-OSS <rafaelpacificodossantos7@gmail.com>
2026-09-08 14:51:18 -0300

    add-Tipo-de-dados-string.js

---
+++ Tipo de dados strings.js
@@ -0,0 +1,3 @@
+let nome = "Ana"; //string com aspas duplas
+let sobrenome = 'Silva'; // string com aspas simples
+let saudacao = -`OLá,  ${nome}!`; // Tamplete literal com interpolaçao de variaveis
