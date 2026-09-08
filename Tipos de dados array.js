2d0f70d5815e24e2a26513d09b0ff5fbd80e4883
Rafapacifico-OSS <rafaelpacificodossantos7@gmail.com>
2026-09-08 15:23:32 -0300

    add-Tipos-de-dados-array.js

---
+++ Tipos de dados array.js
@@ -0,0 +1,3 @@
+let frutas = ["maça", "Banana","Laranja"] // Array de strings
+let numeros = [1, 2, 3, 4, 5]; // Array de numeros
+let misto = [true, "texto", 42]; // Array com difentes tipos de dados 
