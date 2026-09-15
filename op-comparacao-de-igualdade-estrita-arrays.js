8bb43ba700b9145aa721cf37ccc98d7922a30614
Rafapacifico-OSS <rafaelpacificodossantos7@gmail.com>
2026-09-15 16:27:33 -0300

    add-2 op-comparacao-de-igualdade-estrita-arrays.js

---
+++ op-comparacao-de-igualdade-estrita-arrays.js
@@ -0,0 +1,10 @@
+// listaA é um array com 5 elementos 
+let listaA = [10, 20, 30, 40, 50]
+
+// listaB é outro array com os mesmos 5 elementos que a listA
+let listaB = [10, 20, 30, 40, 50]
+
+// listaC recebe uma copia da listaA
+let listaC = listaA;
+
+console.log( listaA === listaB ) // (true)  
