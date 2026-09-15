5e0e189161b235d4d7795da63fcc35262dbf9c35
Rafapacifico-OSS <rafaelpacificodossantos7@gmail.com>
2026-09-15 15:04:55 -0300

    add-op-comparaçao-de-igualdade-arrays.js

---
+++ op-comparaçao-de-igualdade-arrays.js
@@ -0,0 +1,7 @@
+// listaA é um array com 5 elementos 
+let listaA = [10, 20, 30, 40, 50]
+
+// listaB é outro array com os mesmos 5 elementos que a listA
+let listaB = [10, 20, 30, 40, 50]
+
+console.log( listaA == listaB ) // (false) pois mesmo que tenha os mesmo elementos dentro das listas, elas ainda são listas diferentes na memoria 
